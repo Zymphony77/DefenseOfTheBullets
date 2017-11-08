d81bd66fca632adee059c7cad5a10307720c3fb7
Jumpwmk <jumpwmk@Jumpwmks-MacBook-Pro.local>
2017-11-08 14:50:17 +0700

    add class Pair in utility

---
+++ src/utility/Pair.java
@@ -0,0 +1,23 @@
+package utility;
+
+public class Pair 
+{
+	public int first, second;
+	
+	public Pair(int first, int second)
+	{
+		this.first = first;
+		this.second = second;
+	}
+	public Pair()
+	{
+		first = 0;
+		second = 0;
+	}
+	public Pair(Pair tmp)
+	{
+		this.first = tmp.first;
+		this.second = tmp.second;
+	}
+}
+
